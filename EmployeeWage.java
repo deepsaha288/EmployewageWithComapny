ff86d72b3e33106d78f04464bffd2799f7d989f1
Deep Saha <deepsaha288@yahoo.com>
2020-08-28 23:32:17 +0530

    Added employee monthly salary

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,5 +1,6 @@
 public class EmployeeWage
 {
+                //CONSTANTS
         public static final int partTime=1;
 	public static final int fullTime=2;
 	public static final int absent=0;
@@ -10,8 +11,13 @@ public class EmployeeWage
 		//VARIABLES
 		int employeeHour=0;
 		int empWage=0;
+                int totalSalary=0;
+                int salary=0;
+                int workingDays=20;
 
 	 	//Main Logic Build
+                for(int day=1;day<=workingDays;day++)
+                   {
 		int empCheck = (int)( Math.random() * 10 ) % 3;
 
 		                  switch(empCheck)
@@ -30,11 +36,17 @@ public class EmployeeWage
 
 			default:
 		                        System.out.println("invalid choice");
-		}
+		    }
 
 		empWage=employeeHour * empRatePerHrs;
 		System.out.println("Employee Wage:" + empWage);
-	}
+	       }
+             //calculating daily salary
+           salary=(empRatePerHrs*employeeHour);
+
+           //calculating monthly salary
+         salary=(totalSallary+salary);
+        }
 
 	 //main method
     public static void main(String args[]){
@@ -44,7 +56,9 @@ public class EmployeeWage
 
     //method call
     employee.calculatedEmployeeWage();
-    }
+      }
+ 
+
 }
 
 
