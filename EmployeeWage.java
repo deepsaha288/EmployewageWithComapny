ca32f4ab5e668c8323584686c1ac1fabebdefc62
Deep Saha <deepsaha288@yahoo.com>
2020-08-30 15:46:25 +0530

    Added using interface and Sava The Total Wage for Each Comany

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,13 +1,16 @@
 
+//declare interface
+interface employeeWageMethod
+      {
+	public int calculatedEmployeeWage( CompanyEmpWage companyEmployee);
+    }
+//implements interface class
+       public class EmployeeWage implements employeeWageMethod{
 
-public class EmployeeWage {
+	//calculate employee daily wages based on type of employee
 
-
-	/**
-	*calculate employee daily wages based on type of employee
-	*/
-	public int calculatedEmployeeWage(CompanyEmpWage companyEmployee)
-	{
+   public int calculatedEmployeeWage(CompanyEmpWage companyEmployee)
+   {
 		//VARIABLES
 		int totalEmployeeHour=0;
 		int employeeHour=0;
@@ -40,11 +43,12 @@ public class EmployeeWage {
 		//calculated total Salary
 		companyEmployee.setTotalWage(companyEmployee.getEmpRatePerHour() * totalEmployeeHour);
 		}
-                 System.out.println("Total Wage of " + companyEmployee.getCompanyName() + " Employee is " + companyEmployee.getTotalWage());
-		         return totalWage;
+		System.out.println("Total Wage of " + companyEmployee.getCompanyName() + " Employee is " + companyEmployee.getTotalWage());
+		return totalWage;
 		}
 
-	public static void main(String args[]){
+	public static void main(String args[])
+        {
 
     	//created object of class
     	EmployeeWage employeeWage = new EmployeeWage();
@@ -55,16 +59,18 @@ public class EmployeeWage {
     	company[0]=new CompanyEmpWage("TCS",20,100,20);
     	employeeWage.calculatedEmployeeWage(company[0]);
 
-    	company[1]=new CompanyEmpWage("IBM",10,70,20);
+    	company[1]=new CompanyEmpWage("Infosys",10,50,20);
     	employeeWage.calculatedEmployeeWage(company[1]);
 
-    	company[2]=new CompanyEmpWage("Flipkart",10,50,20);
+    	company[2]=new CompanyEmpWage("Wipro",10,30,10);
     	employeeWage.calculatedEmployeeWage(company[2]);
-  	 }
+
+        }
 }
 
 
-class CompanyEmpWage{
+class CompanyEmpWage
+{
      //VARIABLES
      private String companyName;
      private int empRatePerHour;
@@ -73,35 +79,44 @@ class CompanyEmpWage{
      private int totalWage=0;
 
      //CONSTRUCTOR
-     public CompanyEmpWage(final String companyName,final int empRatePerHour,final int maxWorkingHour,final int maxDays){
+     public CompanyEmpWage(final String companyName,final int empRatePerHour,final int maxWorkingHour,final int maxDays)
+     {
        this.companyName=companyName;
        this.empRatePerHour=empRatePerHour;
        this.maxWorkingHour=maxWorkingHour;
        this.maxDays=maxDays;
      }
 
-     public String getCompanyName(){
+     public String getCompanyName()
+     {
        return companyName;
      }
 
-     public int getEmpRatePerHour(){
+     public int getEmpRatePerHour()
+     {
        return empRatePerHour;
      }
 
-     public int getMaxWorkingHour(){
+     public int getMaxWorkingHour()
+     {
        return maxWorkingHour;
      }
 
-     public int getMaxDays(){
+     public int getMaxDays()
+     {
        return maxDays;
      }
 
-     public int getTotalWage(){
+     public int getTotalWage()
+     {
        return totalWage;
      }
 
-	  public void setTotalWage( int totalWage ){
+	  public void setTotalWage( int totalWage )
+          {
 	 	this.totalWage = totalWage;
 	  }
+
+
 }
 
